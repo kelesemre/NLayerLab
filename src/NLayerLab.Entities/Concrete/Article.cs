5b2668263db0e210eefbdb1d1f1e567d19d6586c
kelesemre <emrekeless@gmail.com>
2022-05-09 23:54:56 +0300

    FluentAPI

--- src/NLayerLab.Entities/Concrete/Article.cs
+++ src/NLayerLab.Entities/Concrete/Article.cs
@@ -14,7 +14,7 @@ namespace NLayerLab.Entities.Concrete
         public string Thumbnail { get; set; }
         public DateTime Date { get; set; }
         public int ViewsCount { get; set; }
-        public int CommentsCount { get; set; }
+        public int CommentCount { get; set; }
         public string SeoAuthor { get; set; }
         public string SeoDescription { get; set; }
         public string SeoTags { get; set; }
