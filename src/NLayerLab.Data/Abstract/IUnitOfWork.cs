20908d606cf2b0d3d952e28af477c671b2858910
kelesemre <emrekeless@gmail.com>
2022-05-10 16:28:03 +0300

    FluentAPI+Migration+DatabaseInitialization+UnitOfWork

---
+++ src/NLayerLab.Data/Abstract/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerLab.Data.Abstract
+{
+    public interface IUnitOfWork: IAsyncDisposable
+    {
+        IArticleRepository Articles { get; }
+        ICategoryRepository Categories { get; }
+        ICommentRepository Comments { get; }
+        IRoleRepository Roles { get; }
+        IUserRepository Users { get; } // Instead access to concrete directly we use Interfaces
+        Task<int> SaveAsync(); 
+    }
+}
---
+++ src/NLayerLab.Data/Concrete/UnitOfWork.cs
@@ -0,0 +1,48 @@
+﻿using NLayerLab.Data.Abstract;
+using NLayerLab.Data.Concrete.EntityFramework.Contexts;
+using NLayerLab.Data.Concrete.EntityFramework.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerLab.Data.Concrete
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly NLayerLabContext _context;
+
+        private EfArticleRepository _articleRepository;
+        private EfCategoryRepository _categotyRepository;
+        private EfCommentRepository _commentRepository;
+        private EfRoleRepository _roleRepository;
+        private EfUserRepository _userRepository;
+
+        public UnitOfWork(NLayerLabContext context)
+        {
+            _context = context;
+        }
+
+        // if we need Articles, the program returns initialized object if it is null
+        public IArticleRepository Articles => _articleRepository ?? new EfArticleRepository(_context);
+
+        public ICategoryRepository Categories => _categotyRepository ?? new EfCategoryRepository(_context);
+
+        public ICommentRepository Comments => _commentRepository ?? new EfCommentRepository(_context);
+
+        public IRoleRepository Roles => _roleRepository ?? new EfRoleRepository(_context);
+
+        public IUserRepository Users => _userRepository ?? new EfUserRepository(_context);
+
+        public async ValueTask DisposeAsync()
+        {
+            await _context.DisposeAsync();
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
