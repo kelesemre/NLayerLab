193d257b7ebc7beb783e735e022caf7ed7c670c1
kelesemre <emrekeless@gmail.com>
2022-05-10 22:35:41 +0300

    Result Data

---
+++ src/NLayerLab.Shared/Utilities/Results/Concrete/Result.cs
@@ -0,0 +1,32 @@
+﻿using NLayerLab.Shared.Utilities.Results.Abstract;
+using NLayerLab.Shared.Utilities.Results.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerLab.Shared.Utilities.Results.Concrete
+{
+    public class Result : IResult
+    {
+        public ResultStatus ResultStatus { get; }
+        public string Message { get; }
+        public Exception Exception { get; }
+        public Result(ResultStatus resultStatus)
+        {
+            ResultStatus = resultStatus;
+        }
+        public Result(ResultStatus resultStatus, string message)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+        }
+        public Result(ResultStatus resultStatus, string message, Exception exception)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+            Exception = exception;
+        }
+    }
+}
