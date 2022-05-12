7ecf9a4b266f4479da84e3ec53221e6ed7aedc0a
kelesemre <emrekeless@gmail.com>
2022-05-12 23:52:50 +0300

    Added Dto Objects and Extensions

---
+++ src/NLayerLab.Entities/Dtos/CategoryDto.cs
@@ -0,0 +1,15 @@
+﻿using NLayerLab.Entities.Concrete;
+using NLayerLab.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerLab.Entities.Dtos
+{
+    public class CategoryDto: DtoGetBase
+    {
+        public Category Category { get; set; }
+    }
+}
---
+++ src/NLayerLab.Services/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NLayerLab.Data.Abstract;
+using NLayerLab.Data.Concrete;
+using NLayerLab.Data.Concrete.EntityFramework.Contexts;
+using NLayerLab.Services.Abstract;
+using NLayerLab.Services.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerLab.Services.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection LoadServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDbContext<NLayerLabContext>();
+            serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
+            serviceCollection.AddScoped<ICategoryService, CategoryManager>();
+            serviceCollection.AddScoped<IArticleService, ArticleManager >();
+            return serviceCollection;
+        }
+    }
+}
